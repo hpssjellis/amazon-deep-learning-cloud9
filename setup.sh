3ccb832e7c2cd504b0ed0daaa3e9fc7bf9106305
Jeremy Ellis <keyfreemusic@gmail.com>
2016-05-11 10:58:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ sudo apt-get install g++
 #Cublas Setup
 sudo apt-get install -y  libatlas-base-dev
 
-
+cd /home/ubuntu/workspace
 
 
 #OpenMPI Setup
@@ -24,9 +24,13 @@ sudo apt-get -y install make
 wget http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.2.tar.gz
 tar xvfz openmpi-1.8.2.tar.gz
 cd openmpi-1.8.2
-./configure --prefix=/usr/local/openmpi
+./configure --prefix=/home/ubuntu/workspace/openmpi
 make
 sudo make install
+
+printf "\n\nPATH=\$PATH:\$/home/ubuntu/workspace/openmpi"  >> ~/.profile
+
+
 
 
 
@@ -35,11 +39,11 @@ sudo make install
 wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4/hdf5-1.8.9.tar.gz
 tar xvfz hdf5-1.8.9.tar.gz
 cd hdf5-1.8.9
-./configure --prefix=/usr/local
+./configure --prefix=/home/ubuntu/workspace/hdf5
 make
 sudo make install
 
-
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/hdf5"  >> ~/.profile
 
 
 
@@ -48,32 +52,44 @@ sudo make install
 wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4/zlib-1.2.8.tar.gz
 tar xvf zlib-1.2.8.tar.gz
 cd zlib-1.2.8
-./configure
+./configure --prefix=/home/ubuntu/workspace/zlib
 make
 sudo make install
+
+
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/zlib"  >> ~/.profile
+
+
+
 
 
 #Netcdf Setup
 wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.1.3.tar.gz
 tar xvf netcdf-4.1.3.tar.gz
 cd netcdf-4.1.3
-./configure --prefix=/usr/local
+./configure --prefix=/home/ubuntu/workspace/netcdf
 make
 sudo make install
+
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/netcdf"  >> ~/.profile
+
+
+
 
 
 #Netcdfc++ Setup
 wget http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz
 tar xvf netcdf-cxx4-4.2.tar.gz
 cd netcdf-cxx4-4.2
-./configure --prefix=/usr/local
+./configure --prefix=/home/ubuntu/workspace/netcdfcpp
 make
 sudo make install
 
-
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/netcdfcpp"  >> ~/.profile
 
 
 #JsonCPP Setup
+cd /home/ubuntu/workspace
 
 sudo apt-get -y install cmake
 wget https://github.com/open-source-parsers/jsoncpp/archive/svn-import.tar.gz
@@ -86,21 +102,35 @@ make
 sudo make install
 
 
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/jsoncpp-svn-import"  >> ~/.profile
+
+
+
 #CUB Setup
+cd /home/ubuntu/workspace
 
 wget https://github.com/NVlabs/cub/archive/1.5.2.zip
 sudo apt-get install -y  unzip
 unzip 1.5.2.zip
-sudo cp -rf cub-1.5.2/cub/ /usr/local/include/
+sudo cp -rf cub-1.5.2/cub/ /home/ubuntu/workspace/include/
+
+
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/include"  >> ~/.profile
+
 
 
 #Download the code and build.
+cd /home/ubuntu/workspace
 
 git clone https://github.com/amznlabs/amazon-dsstne.git
 cd amazon-dsstne/src/amazon/dsstne
 #Add the mpiCC and nvcc compiler in the path
-export PATH=/usr/local/openmpi/bin:/usr/local/cuda/bin:$PATH
+export PATH=/home/ubuntu/workspace/openmpi/bin:/home/ubuntu/workspace/cuda/bin:$PATH
 make
+
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/amazon-dsstne/src/amazon/dsstne"  >> ~/.profile
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/openmpi/bin"  >> ~/.profile
+printf "\nPATH=\$PATH:\$/home/ubuntu/workspace/cuda/bin"  >> ~/.profile
 
 
 
@@ -108,17 +138,34 @@ echo "Try the examples at "
 echo "amazon-dsstne/docs/getting_started/examples.md"
 echo "or"
 echo "https://github.com/amznlabs/amazon-dsstne/blob/master/docs/getting_started/examples.md"
-export PATH=`pwd`/bin:$PATH
+
+
+
+# what is this line doing
+#export PATH=`pwd`/bin:$PATH
+
+
+#example how to setup a profile for new terminals
+#printf "\nPATH=\$PATH:\$GRADLE_USER_HOME/bin"  >> ~/.profile
 
 
 
 
+
+
+
+echo "All done setup "
+echo "-------------------------------------------"
+
+
+cd /home/ubuntu/workspace
 # some examples
-wget https://s3-us-west-2.amazonaws.com/amazon-dsstne-samples/data/ml20m-all
-generateNetCDF -d gl_input -i ml20m-all -o gl_input.nc -f features_input -s samples_input -c
-generateNetCDF -d gl_output -i ml20m-all -o gl_output.nc -f features_output -s samples_input -c
+#wget https://s3-us-west-2.amazonaws.com/amazon-dsstne-samples/data/ml20m-all
+#generateNetCDF -d gl_input -i ml20m-all -o gl_input.nc -f features_input -s samples_input -c
+#generateNetCDF -d gl_output -i ml20m-all -o gl_output.nc -f features_output -s samples_input -c
 
-test.what
+#test.c
 
-train -c config.json -i gl_input.nc -o gl_output.nc -n gl.nc -b 256 -e 10
+#train -c config.json -i gl_input.nc -o gl_output.nc -n gl.nc -b 256 -e 10
+
 
